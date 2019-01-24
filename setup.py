d2ce6eed92f93be6ee04b5a261455c63ca454acb
Tetsu Haruyama <tetsu.yes@gmail.com>
2019-01-24 14:52:46 +0900

    additional files corrected

--- setup.py
+++ setup.py
@@ -1,21 +1,27 @@
-import setuptools
+import glob
+import os
+import sys
+from setuptools import find_packages, setup
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-setuptools.setup(
-     name='Wooldridge',
-     version='0.1',
-     scripts=['wooldridge'] ,
-     author="Tetsu Haruyama",
-     author_email="tetsu.yes@gmail.com",
-     description="Data sets for Introductory Econometrics: A Modern Approach (6th ed, J.M. Wooldridge)",
-     long_description=long_description,
-   long_description_content_type="text/markdown",
-     url="https://github.com/spring-haru/wooldridge",
-     packages=setuptools.find_packages(),
-     classifiers=[
-         "Programming Language :: Python :: 3",
-         "License :: GPLv3",
-         "Operating System :: OS Independent",
-     ],
- )
+
+additional_files = []
+for filename in glob.iglob('./wooldridge/**', recursive=True):
+    if '.csv.bz' in filename:
+        additional_files.append(filename.replace('./wooldridge/', ''))
+
+setup(
+    name='wooldridge',
+    version='0.1.0',
+    author='Tetsu Haruyama',
+    author_email='tetsu.yes@gmail.com',
+    packages=find_packages(),
+    # packages=['wooldridge'],
+    package_dir={'wooldridge': './wooldridge'},
+    include_package_data=True,
+    package_data={'wooldridge': additional_files},
+    url='https://github.com/spring-haru/wooldridge',
+    license='LICENSE',
+    description='Data sets for Introductory Econometrics: A Modern Approach (6th ed, J.M. Wooldridge)',
+    long_description=open('README.md').read(),
+    install_requires=[],
+)
